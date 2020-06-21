29031b19c80d71db3adedef397d107c94b38bfef
Yu-TingWang <yutingwang.wang@mail.utoronto.ca>
2020-06-21 15:00:35 -0400

    cache code

--- DS/BinaryTrie/__init__.py
+++
@@ -1,8 +0,0 @@
-class BinaryTrie:
-    def __init__(self,x):
-        self.child=[None,None]
-        # self.child[0] is the left child
-        # self.child[1] is the right child
-        self.jump = self.prev = self.next = None
-        self.val = x
-
--- DS/XFastTrie/__init__.py
+++
@@ -1,5 +0,0 @@
-from DS.BinaryTrie import BinaryTrie
-class XFastTrie(BinaryTrie):
-    class Node():
-        def __init__(self,x):
-            super(self,x)
